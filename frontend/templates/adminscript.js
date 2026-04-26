3c8995149ece93bf2188935314a31e73025cfdec
Nate Wong <nwwong17@gmail.com>
2026-04-26 02:09:35 -0700

    linked frontend pages, edited page format
    just need to link frontend to python scripts that show the list of degree files

---
+++ frontend/templates/adminscript.js
@@ -0,0 +1,7 @@
+document.getElementById("degreeUpDiv").style.display = "none";
+
+var gradeDropdown = document.getElementById("gradeFiles");
+var degreeDropdown = document.getElementById("degreeFiles");
+
+//TODO: link up to python script to get list of files in grade and degree folders and populate dropdowns with those files
+      
