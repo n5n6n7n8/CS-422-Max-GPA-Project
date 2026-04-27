034e122ce53c3b0c5eeaca5fe4ece34ca3183250
Nate Wong <nwwong17@gmail.com>
2026-04-26 17:36:44 -0700

    adjusted background color
    small fix

--- frontend/templates/adminscript.js
+++ frontend/templates/adminscript.js
@@ -1,7 +1,9 @@
+//This code hides the Degree Upload Div container so only the Grade Upload is shown upon loading the page.
 document.getElementById("degreeUpDiv").style.display = "none";
 
 var gradeDropdown = document.getElementById("gradeFiles");
 var degreeDropdown = document.getElementById("degreeFiles");
+
 
 //TODO: link up to python script to get list of files in grade and degree folders and populate dropdowns with those files
       
